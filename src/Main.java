0ef77e52edd9e9b3f18fb937d4143d5e900aa118
grafo <grafonov2003@inbox.ru>
2024-05-15 01:42:30 +0500

    Dz8 Gotovoe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Задача 1");
         int[] number = new int[3];
         number[0] = 1;
@@ -18,6 +19,7 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println(own[i]);
         }
+
         System.out.println("Задача 2");
         int[] car = {1, 5, 15, 30};
         int[] airplane = {2, 6, 12, 24};
@@ -38,31 +40,29 @@ public class Main {
                 System.out.println();
             }
         }
+
         System.out.println("Задача 3");
         int[] pumber = new int[3];
         pumber[0] = 1;
         pumber[1] = 2;
         pumber[2] = 3;
-        System.out.print(" " + pumber[2]+ " ");
-        System.out.print(" " +pumber[1]+ " ");
-        System.out.print(" " +pumber[0]+ " ");
+        System.out.print(" " + pumber[2] + " ");
+        System.out.print(" " + pumber[1] + " ");
+        System.out.print(" " + pumber[0] + " ");
         System.out.println();
         double[] fractionalPumber = {1.57, 7.654, 9.986};
-        System.out.print(" " + fractionalPumber[2]+ " ");
-        System.out.print(" " + fractionalPumber[1]+ " ");
-        System.out.print(" " + fractionalPumber[0]+ " ");
+        System.out.print(" " + fractionalPumber[2] + " ");
+        System.out.print(" " + fractionalPumber[1] + " ");
+        System.out.print(" " + fractionalPumber[0] + " ");
         System.out.println();
 
         System.out.println("Задача 4");
-        int [] massif = new int []{1, 3, 35};
-        for (int x = 0; x < massif.length; x++){
-            if (massif[x] % 2 != 0){
+        int[] massif = new int[]{1, 3, 35};
+        for (int x = 0; x < massif.length; x++) {
+            if (massif[x] % 2 != 0) {
                 massif[x] = massif[x] + 1;
             }
             System.out.print(" " + massif[x] + " ");
         }
-
-
-
     }
 }
